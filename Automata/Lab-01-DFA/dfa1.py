3dafe36ea4a4a57f6793dc047483a9e8196b0e75
Aliza Khan <124785215+AlizaKhan3@users.noreply.github.com>
2025-03-19 01:50:26 +0500

    Add files via upload

---
+++ Automata/Lab-01-DFA/dfa1.py
@@ -0,0 +1,29 @@
+class DFA:
+    def __init__(self, num_states, input_chars, initial_state, final_states, transition_table):
+        self.num_states = num_states  
+        self.input_chars = input_chars  
+        self.initial_state = initial_state  
+        self.final_states = final_states
+        self.transition_table = transition_table  
+
+    def validate(self, word):
+        current_state = self.initial_state
+        for char in word:
+            if char not in self.input_chars:  
+                raise ValueError(f"Invalid character '{char}' in input. Valid characters are: {self.input_chars}")
+            char_index = self.input_chars.index(char)
+            current_state = self.transition_table[current_state][char_index]
+        return current_state in self.final_states
+
+input_chars = ['a', 'b']  
+final_states = [2, 3]  
+transition_table = [[1, 4], [3, 2],[2, 2],[3, 3],[4, 4]]
+dfa = DFA(5, input_chars, 0, final_states, transition_table)
+
+print("DFA of (ab+aa)(a+b)*: String that starts with either 'ab' OR with 'aa'.")
+word = input('Input: ')
+try:
+    print("Accepted" if dfa.validate(word) else "Rejected")
+except ValueError as e:
+    print(e)
+    
